b370d421017131c83f1eeadf1fdde8164f1f4ff0
Jonas Paiva <jonaspwork@gmail.com>
2022-07-13 18:29:03 -0300

    Feat: Adicionado Requisito n19

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const enviarBtn = document.getElementById("enviar");
-const checkbox = document.getElementById("agreement");
-const submitBnt = document.getElementById("submit-btn");
+const enviarBtn = document.getElementById('enviar');
+const checkbox = document.getElementById('agreement');
+const submitBnt = document.getElementById('submit-btn');
 
 function enviar() {
-  const email = document.getElementById("email");
-  const password = document.getElementById("password");
-  if (email.value === "tryber@teste.com" && password.value === "123456") {
-    alert("Olá, Tryber!");
+  const email = document.getElementById('email');
+  const password = document.getElementById('password');
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
 
 function disabledBtn() {
-  if (submitBnt.hasAttribute("disabled") == true) {
-    submitBnt.removeAttribute("disabled");
+  if (submitBnt.hasAttribute('disabled') === true) {
+    submitBnt.removeAttribute('disabled');
   } else {
-    submitBnt.setAttribute("disabled","disabled");
+    submitBnt.setAttribute('disabled', 'disabled');
   }
 }
 
-checkbox.addEventListener("click", disabledBtn);
+checkbox.addEventListener('click', disabledBtn);
 
-enviarBtn.addEventListener("click", enviar);
+enviarBtn.addEventListener('click', enviar);
